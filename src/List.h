6a2065109a170c8003842ef0da5e29488ea61a31
Mitchell Riley <mitchell.v.riley@gmail.com>
2009-03-24 17:56:46 +1000

    Change jgList from linked list to array. Phew.

--- src/List.h
+++ src/List.h
@@ -7,19 +7,12 @@
 
 #include <stdbool.h>
 
-typedef struct jgListNode
-{
-     void *data;
-     struct jgListNode *next;
-} jgListNode;
-
 typedef struct jgList
 {
-     jgListNode *head;
-     int length;
+     void **arr;
+     int    length;    // Number of elements
+     int    alloc;     // Allocated size
 } jgList;
-
-typedef void  (*jgListIter)(void *data, void *mydata);
 
 extern jgList  *jgListNew();
 extern void     jgListClear(jgList *list);
@@ -32,46 +25,31 @@ extern void     jgListInsert(jgList *list, int position, void *data);
 extern void     jgListDelete(jgList *list, int position);
 
 extern bool     jgListContains(jgList *list, void *data);
-extern bool     jgListHasLoop(jgList *list);
 
 extern void     jgListAdd(jgList *list, void *data);
 extern void     jgListRemove(jgList *list, void *data);
 
 // These are clunky but they'll do for now.
 
-#define JG_LIST_FOREACH(list, var)                              \
-     var = NULL;                                                \
-     if(list->length > 0) var = list->head->data;               \
-     for(jgListNode *_c_ = list->head;                          \
-         _c_ != NULL;                                           \
-         _c_ = _c_->next, var = (_c_ == NULL) ? NULL : _c_->data)
+#define JG_LIST_FOREACH(list, var)              \
+     var = jgListGet(list, 0);                  \
+     for(int _i_ = 0; _i_ < list->length; _i_++, var = jgListGet(list, _i_))
 
 #define JG_LIST_FOREACH_PAIR(list, var, var2)                           \
-     var = NULL;                                                        \
-     if(list->length > 1) var = list->head->data, var2 = list->head->next->data; \
-     for(jgListNode *_c_ = list->head;                                  \
-         _c_ != NULL;                                                   \
-         _c_ = _c_->next,                                               \
-              var = (_c_ == NULL) ? NULL : _c_->data,                   \
-              var2 = (_c_ == NULL) ? NULL :                             \
-              ((_c_->next == NULL) ? jgListGet(list, 0)  : _c_->next->data) \
-          )
+     var = jgListGet(list, 0);                                          \
+     var2 = jgListGet(list, 1);                                         \
+     for(int _i_ = 0; _i_ < list->length; _i_++,                        \
+              var = jgListGet(list, _i_),                               \
+              var2 = (_i_ == list->length) - 1 ? jgListGet(list, 0) : jgListGet(list, _i_))
+          
 
-#define JG_LIST_FOREACH_COMBO(b)                                        \
+#define JG_LIST_FOREACH_COMBO(list, b)                                  \
      b = NULL;                                                          \
-     if(_c_->next != NULL) b = _c_->next->data;                         \
-     for(jgListNode *_c2_ = _c_->next;                                  \
-         _c2_ != NULL;                                                  \
-         _c2_ = _c2_->next, b = (_c2_ == NULL) ? NULL : _c2_->data)
-
+     if(_i_ != list->length - 1) b = jgListGet(list, _i_ + 1);          \
+     for(int _i2_ = _i_ + 1; _i2_ < list->length; _i2_++, b = (_i2_ == list->length) ? NULL : jgListGet(list, _i2_))
 
 // Can pair with themselves.
 
 /* #define JG_LIST_FOREACH_COMBO(b)                                         \ */
-/*      b = NULL;                                                          \ */
-/*      if(_c_ != NULL) b = _c_->data;                                     \ */
-/*      for(jgListNode *_c2_ = _c_;                                        \ */
-/*          _c2_ != NULL;                                                  \ */
-/*          _c2_ = _c2_->next, b = (_c2_ == NULL) ? NULL : _c2_->data) */
 
 #endif
